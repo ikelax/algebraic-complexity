1fa276a5f53ba7d55700c4ee569640fcd33ad14e
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-19 22:38:35 +0100

    Add examples for Formula.Neg

--- AlgebraicComplexity/Example.lean
+++ AlgebraicComplexity/Example.lean
@@ -25,11 +25,13 @@ def formula6 : Formula ℝ :=
 
 example : size (.Const 1) = 0 := by rfl
 example : size (.Var "") = 0 := by rfl
+example : size (.Neg (.Const 1)) = 1 := by rfl
 example : size (.Add (.Const 1) (.Const 0)) = 1 := by rfl
 example : size (.Add (.Mult (.Const (-3.4)) (.Add (.Const 0) (.Var "y"))) (.Add (.Var "x") (.Const 1))) = 4 := by rfl
 -- example : size - (Formula.Const 1) * (Formula.Var "x") + (Formula.Var "y") = 2 := by rfl
 
 example : depth (.Const 1) = 1 := by rfl
 example : depth (.Var "x") = 1 := by rfl
+example : depth (.Neg (.Const 1)) = 2 := by rfl
 example : depth (.Add (.Const 1) (.Const 1)) = 2 := by rfl
 example : depth (.Add (.Mult (.Const (-3.4)) (.Add (.Const 0) (.Var "y"))) (.Add (.Var "x") (.Const 1))) = 4 := by rfl
