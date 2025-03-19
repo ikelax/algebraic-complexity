c5cf49e4d0d8c5a01ec931070c8fcc83e71a1a74
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-19 22:36:20 +0100

    Add more examples

--- AlgebraicComplexity/Example.lean
+++ AlgebraicComplexity/Example.lean
@@ -1,18 +1,35 @@
 import AlgebraicComplexity.Formulas
 
 def formula1 : Formula ℝ :=
- Formula.Const 0
+  Formula.Const 0
 
 def formula2 : Formula ℝ :=
- Formula.Const 1
+  Formula.Const 1
 
 def formula3 : Formula ℝ :=
- Formula.Var "variable"
+  Formula.Var "variable"
 
 def formula4 : Formula ℝ :=
- Formula.Add (.Const 1) (.Mult (.Const 1) (.Var "x"))
+  Formula.Add (.Const 1) (.Mult (.Const 1) (.Var "x"))
+
+def formula5 : Formula ℝ :=
+  Formula.Neg (.Const 1)
+
+def formula6 : Formula ℝ :=
+  - (.Const 1) * (.Var "x") + (.Var "y")
+
+-- def formula7 : Formula ℝ :=
+  -- -1 * 3 + 5
+
+-- strings and identifiers
 
 example : size (.Const 1) = 0 := by rfl
 example : size (.Var "") = 0 := by rfl
 example : size (.Add (.Const 1) (.Const 0)) = 1 := by rfl
 example : size (.Add (.Mult (.Const (-3.4)) (.Add (.Const 0) (.Var "y"))) (.Add (.Var "x") (.Const 1))) = 4 := by rfl
+-- example : size - (Formula.Const 1) * (Formula.Var "x") + (Formula.Var "y") = 2 := by rfl
+
+example : depth (.Const 1) = 1 := by rfl
+example : depth (.Var "x") = 1 := by rfl
+example : depth (.Add (.Const 1) (.Const 1)) = 2 := by rfl
+example : depth (.Add (.Mult (.Const (-3.4)) (.Add (.Const 0) (.Var "y"))) (.Add (.Var "x") (.Const 1))) = 4 := by rfl
