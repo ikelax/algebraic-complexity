173e46ba5b4160eac9579f3f569ba2fcfa9ad65a
ikelax <163678144+ikelax@users.noreply.github.com>
2025-03-25 17:40:08 +0100

    Add basics for formulas (#3)
    * Add Formula and size
    
    They are still buggy.
    
    * Lots of fixes
    
    * Fix definition of size
    
    The issues where mostly syntactical.
    
    * Add a few examples for Formula and size
    
    * Replace checks with definitions
    
    * Prove examples for size
    
    * Fix build-project workflow
    
    The project was renamed to AlgebraicComplexity.
    
    * Add definition for depth
    
    * Delete project folder
    
    The files were moved to AlgebraicComplexity.
    
    * Add more examples
    
    * Add examples for Formula.Neg
    
    * some fixes
    
    * Use new notation for examples
    
    * Add draft version of evalToPolynomial
    
    * Coerce constants to polynomial and add examples
    
    * Ignore Test.lean
    
    I want to use this file for testing stuff in Lean.
    
    * Implement evalToPolynomial further
    
    We require multivariate polynomials and thus should use MvPolynomial.
    Multivariate polynomials are required because the complexities we
    measure are a function of the number of variables.
    
    CommSemiring is required because MvPolynomial requires CommSemiRing.
    CommRing is required because semirings are rings dropping the
    requirement that each element must have an additive inverse.
    
    * Start fixing the examples for evalToPolynomial
    
    * Switch CommSemiring back to Ring
    
    I works also with Ring. I do not know what I did wrong earlier.
    
    * Introduce n for Fin n
    
    * Push fixes
    
    * Fix example for evalToPolynomial
    
    ---------
    
    Co-authored-by: Shreyas <shreyasss94@gmail.com>
    Related to: https://github.com/ikelax/algebraic-complexity/issues/2

---
+++ AlgebraicComplexity/Example.lean
@@ -0,0 +1,46 @@
+import AlgebraicComplexity.Formulas
+import Mathlib
+
+open MvPolynomial
+
+def formula1 : Formula ℝ 3 := C[0]
+
+def formula2 : Formula ℝ 0 := C[1]
+
+def formula3 : Formula ℝ 1 := V[0]
+
+def formula4 : Formula ℝ 1 := C[1] + C[1] * V[0]
+
+def formula5 : Formula ℝ 5 := -C[1]
+
+def formula6 : Formula ℝ 21 := -C[1] * V[4] + V[20] + V[0]
+
+def formula7 : Formula ℝ 7 := -C[1] * C[3] + C[5]
+
+example : @size ℝ 1 C[1] = 0 := by rfl
+example : @size Real 1 V[0] = 0 := by rfl
+example : @size ℤ 2 (-C[1]) = 1 := by rfl
+example : @size ℚ 2 (V[0] + C[1]) = 1 := by rfl
+example : @size ℚ 2 (C[1] + C[0]) = 1 := by rfl
+example : @size ℝ 3 (C[-3.4] * (C[0] + V[2]) + V[1] + C[1]) = 4 := by rfl
+example : @size ℝ 3 (C[-3.4] * (C[0] + V[2]) + (V[1] + C[1])) = 4 := by rfl
+example : @size ℤ 3 (-C[3] * (V[1] + V[2])) = 3 := by rfl
+
+example : @depth ℝ 1 C[1] = 0 := by rfl
+example : @depth Real 1 V[0] = 0 := by rfl
+example : @depth ℤ 2 (-C[1]) = 1 := by rfl
+example : @depth ℚ 2 (V[1] + C[1]) = 1 := by rfl
+example : @depth ℚ 2 (C[1] + C[0]) = 1 := by rfl
+example : @depth ℝ 3 (C[-3.4] * (C[0] + V[2]) + V[1] + C[1]) = 4 := by rfl
+example : @depth ℝ 3 (C[-3.4] * (C[0] + V[2]) + (V[1] + C[1])) = 3 := by rfl
+example : @depth ℤ 3 (-C[3] * (V[1] + V[2])) = 2 := by rfl
+
+example : @evalToPolynomial ℝ 1 _ (C[1]) = 1 := by rfl
+example : @evalToPolynomial Real 1 _ V[0] = X 1 := by rfl
+example : @evalToPolynomial ℤ 2 _ (-C[1]) = -1 := by rfl
+example : @evalToPolynomial ℚ 2 _ (V[1] + C[1]) = X 1 + 1 := by rfl
+example : @evalToPolynomial ℚ 2 _ (C[1] + C[0]) = 1 + 0 := by simp[evalToPolynomial]
+example : @evalToPolynomial ℝ 3 _ (C[-3] * (C[0] + V[2]) + V[1] + C[1]) =
+    (C (-3)) * (0 + (X 2)) + (X 1) + 1 := by simp
+example : @evalToPolynomial ℚ  3 _ (C[-3] * (C[0] + V[2]) + (V[1] + C[1])) = (C (-3)) * (0 + (X 2)) + ((X 1) + 1) := by simp
+example : @evalToPolynomial ℤ 3 _ (-C[3] * (V[1] + V[2])) = -3 * (X 1 + X 2) := by rfl
---
+++ AlgebraicComplexity/Formulas.lean
@@ -0,0 +1,55 @@
+import Mathlib
+
+open MvPolynomial
+
+inductive Formula (α : Type u) (n : ℕ) where
+| Var (x: Fin n)
+| Add (g h: Formula α n): Formula α n
+| Mult (g h: Formula α n): Formula α n
+| Neg (g : Formula α n): Formula α n
+| Const (c : α): Formula α n
+
+notation "C[" val "]" => Formula.Const val
+notation "V[" name "]" =>  Formula.Var ⟨name, by decide⟩
+instance zero [Ring α]: Zero (Formula α n) where
+  zero := .Const 0
+
+instance one [Ring α]: One (Formula α n) where
+  one := .Const 1
+
+instance add [Ring α]: Add (Formula α n) where
+  add := .Add
+
+instance neg [Ring α] : Neg (Formula α n) where
+  neg := .Neg
+
+instance sub [Ring α] : Sub (Formula α n) where
+  sub a b := a + (- b)
+
+instance mul' [Ring α] : Mul (Formula α n) where
+  mul := .Mult
+
+def size (f: Formula α n) : ℕ :=
+match f with
+| .Var _ => 0
+| .Add g h => size g + size h + 1
+| .Mult g h => size g + size h + 1
+| .Neg g => size g + 1
+| .Const _ => 0
+
+def depth (f: Formula α n) : ℕ :=
+match f with
+| .Var _ => 0
+| .Add g h => max (depth g) (depth h) + 1
+| .Mult g h => max (depth g) (depth h) + 1
+| .Neg g => depth g + 1
+| .Const _ => 0
+
+@[simp]
+noncomputable def evalToPolynomial [CommRing α] (f: Formula α n) :  (MvPolynomial (Fin n) α) :=
+match f with
+| .Var x => X x
+| .Add g h => evalToPolynomial g + evalToPolynomial h
+| .Mult g h => evalToPolynomial g * evalToPolynomial h
+| .Neg g => - evalToPolynomial g
+| .Const c => MvPolynomial.C c
